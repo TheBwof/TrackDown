c4f53771caf5a8e253aeaff96511526570ee3403
Fahim Ahamed <121462098+TheBwof@users.noreply.github.com>
2024-01-26 22:58:02 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,4 @@ module.exports = {
    botToken = "YOUR_BOT_TOKEN", // Modify your bot TOKEN here
    hostURL = "YOUR_URL", // Modify your URL here
 };
+// for port forewording use "ssh -R 80:localhost:5000 serveo.net"
