39b7ac60d00c7f8fea8ffdf70a723d117f92f08f
Fahim Ahamed <121462098+TheBwof@users.noreply.github.com>
2024-01-26 22:54:43 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 module.exports = {
-    botToken2: "6691480482:AAF4ztaS-cBkA8QwMnhfqx7WAfmlJV0Sk5U",
-    chatId2: "5630435349",
+   botToken = "YOUR_BOT_TOKEN", // Modify your bot TOKEN here
+   hostURL = "YOUR_URL", // Modify your URL here
 };
